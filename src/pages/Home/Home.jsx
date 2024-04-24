f76cb69f3bbfc5de1fa21aae1066c6ed0457421d
Azimjonov08 <azimjonovm008@gmail.com>
2024-04-24 22:35:05 +0500

    remove scrool y

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,11 @@ import Header from "../../components/Header/Header"
 import Hero from "../../components/Hero/hero"
 import Who from "../../components/Who/Who"
 import Skils from "../../components/Skils/Skils"
-
+import "./Home.scss"
 
 function Home() {
   return (
-    <>
-
+    < div className="home">
       <Header />
       <Hero />
       <Who />
