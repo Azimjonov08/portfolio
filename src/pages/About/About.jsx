350233ef4146e4ddc27da372045b221f724e504d
Azimjonov08 <azimjonovm008@gmail.com>
2024-05-05 18:43:53 +0500

    update about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,29 +6,32 @@ function About() {
     return (
         <section className="about">
             <Header />
-            <div className="container box">
-                <h2 className="title">
-                    About Me
-                </h2>
-                <p>
-                    I am Azimjonov Muhammadamin, 17 years old, software developer, Tashkent, Uzbekistan, currently living in Andijan, Uzbekistan.
+            <div className="container ">
+                <div className="box">
+                    <h2 className="title">
+                        About Me
+                    </h2>
+                    <p>
+                        I am Azimjonov Muhammadamin, 17 years old, software developer, Tashkent, Uzbekistan, currently living in Andijan, Uzbekistan.
 
-                </p>
-                <p>
-                    I have been doing web development for 4 months. During this time I gained valuable experience and skills in various aspects of web development. I have been involved in many projects for the past 3 months.
-                </p>
-                <p>
-                    If you want to get in touch, I'm very responsive via email and I'm also very active on Telegram.
-                </p>
+                    </p>
+                    <p>
+                        I have been doing web development for 4 months. During this time I gained valuable experience and skills in various aspects of web development. I have been involved in many projects for the past 3 months.
+                    </p>
+                    <p>
+                        If you want to get in touch, I'm very responsive via email and I'm also very active on Telegram.
+                    </p>
 
-                <p>
-                    I am happy to share my experiences in web development with you. So far I have been working as a freelancer. Through these experiences, I gained a deeper understanding that facilitated the learning and acquisition of various knowledge and experiences.
-                </p>
+                    <p>
+                        I am happy to share my experiences in web development with you. So far I have been working as a freelancer. Through these experiences, I gained a deeper understanding that facilitated the learning and acquisition of various knowledge and experiences.
+                    </p>
 
-                <p>
-                    If you're interested in seeing what I've been up to, you can check out my <a href="https://t.me/my_day_develop">Portfolio</a> or my   <a href="https://github.com/Azimjonov08"> GitHub</a>  account. There,you'll find many of the projects I've worked on, which will give you a closer look at what I do.
-                </p>
-                <p className="p">Best regards 🙂, Muhammadamin</p>
+                    <p>
+                        If you're interested in seeing what I've been up to, you can check out my <a href="https://t.me/my_day_develop">Portfolio</a> or my   <a href="https://github.com/Azimjonov08"> GitHub</a>  account. There,you'll find many of the projects I've worked on, which will give you a closer look at what I do.
+                    </p>
+                    <p className="p">Best regards 🙂, Muhammadamin</p>
+                </div>
+
             </div>
             <Footer />
         </section>
